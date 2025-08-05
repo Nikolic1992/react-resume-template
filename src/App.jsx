febe18cfe3db69b5a72b7bb4af93ce3597e43f8f
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-08-05 21:04:27 +1000

    feat: add responsive footer component with newsletter subscription

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import MyWork from "./components/MyWork/MyWork";
 import Navbar from "./components/NavBar/NavBar";
@@ -14,6 +15,7 @@ function App() {
       <Services />
       <MyWork />
       <Contact />
+      <Footer />
     </div>
   );
 }
