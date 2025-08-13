d7b95f7a0736ea36e5729ce20aa24ead2f38a5d9
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-08-13 20:37:45 +1000

    feat: add smooth scroll navigation with anchor links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 function Hero() {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
         <span>I'm John Snow,</span> Frontend Developer based in Castle Black
@@ -12,7 +13,11 @@ function Hero() {
         the latest web technologies. Let's build something amazing together!
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My Resume</div>
       </div>
     </div>
