e30a3c4743e6ebfd0f1a674a42f47e5e8c479935
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-08-17 13:02:14 +1000

    feat: add email form submit with web3 forms

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,29 @@ import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
 
 function Contact() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_EMAIL_API_KEY);
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact-title">
@@ -35,7 +58,7 @@ function Contact() {
           </div>
         </div>
 
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name..." name="name" />
           <label htmlFor="">Your Email</label>
