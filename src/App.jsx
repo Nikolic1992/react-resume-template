141d1075ea74a68a5fbc24b1dca87c331112edef
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-08-21 21:33:53 +1000

    final fix Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import MyWork from "./components/MyWork/MyWork.jsx";
-import Navbar from "./components/NavBar/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import Services from "./components/Services/Services.jsx";
 
 function App() {
